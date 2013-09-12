d1797f4e538be32c8a5408c95325b0dee8803675
Jessie Smith <jessicagraysmith@gmail.com>
2013-09-12 07:03:10 -0400

    Created array of UIImageViews that contain each board piece as a UIImage. Each UIImageView is half the size of the UIImage since the UIImage is a high resolution (for retina display) image.

--- Pentominoes/JGSViewController.m
+++ Pentominoes/JGSViewController.m
@@ -13,6 +13,7 @@
 - (IBAction)newBoardSelected:(id)sender;
 
 @property NSInteger currentBoard;
+@property NSMutableArray *boardPieces;
 @end
 
 @implementation JGSViewController
@@ -21,6 +22,22 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.boardPieces = [[NSMutableArray alloc] init];
+    NSArray *boardPieceLetters = [NSArray arrayWithObjects:@"F", @"I", @"L", @"N", @"P", @"T", @"U", @"V", @"W", @"X", @"Y", @"Z", nil];
+    
+    // Create mutable array of board pieces that are each UIImageViews half the size of their corresponding UIImage
+    for(NSUInteger i = 0; i < [boardPieceLetters count]; i++) {
+        
+        UIImage *pieceImage = [UIImage imageNamed: [NSString stringWithFormat:@"tile%@.png", boardPieceLetters[i]]];
+        UIImageView *boardPiece = [[UIImageView alloc] initWithImage:pieceImage];
+        
+        CGSize pieceImageSize = pieceImage.size;
+        boardPiece.frame = CGRectMake(0, 0, pieceImageSize.width/2, pieceImageSize.height/2);
+        
+        [self.boardPieces addObject:boardPiece];
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning
